fd674868414a10f9ae015a4a0c5f2a0df6607058
awhb <awhangbin@gmail.com>
2025-10-09 07:04:47 +0800

    feat: touchup navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -19,7 +19,7 @@ export default function NavBar() {
 
   return (
     <header className="sticky top-0 w-full border-b border-gray-200 bg-navbar">
-      <div className="container mx-auto flex-auto h-auto items-center justify-between px-4">
+      <div className="container mx-auto flex h-auto items-center justify-between px-4">
         {/* Left: Logo / Header */}
         <div className="flex items-center text-2xl sm:text-3xl lg:text-4xl sm:p-2 md:p-4">
           <NavLink to="/" className="font-pixelify-sans">
@@ -28,29 +28,29 @@ export default function NavBar() {
         </div>
         {/* Right: Navigation (only if authenticated) */}
         { isAuthenticated && (
-          <nav className="flex items-center space-x-6 text-sm font-medium">
+          <nav className="flex flex-1 items-center justify-around space-x-6 text-md sm:text-large lg:text-xl">
             <NavLink to="/match" className={({isActive}) => isActive ? activeLinkClasses : linkClasses}>
               {({isActive}) => (
-                <>
+                <div className='container font-press-start-2p'>
                   {isActive && <ArrowIndicator />}
-                  MATCH
-                </>
+                  MATCH!
+                </div>
               )}
             </NavLink>
             <NavLink to="/" className={({isActive}) => isActive ? activeLinkClasses : linkClasses}>
               {({isActive}) => (
-                <>
+                <div className='container'>
                   {isActive && <ArrowIndicator />}
                   Dashboard
-                </>
+                </div>
               )}
             </NavLink>
             <NavLink to="/profile" className={({isActive}) => isActive ? activeLinkClasses : linkClasses}>
               {({isActive}) => (
-                <>
+                <div className='container'>
                   {isActive && <ArrowIndicator />}
                   Profile
-                </>
+                </div>
               )}
             </NavLink>
           </nav>
