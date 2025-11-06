c4f282d6f45763e4a9edd3b72cf53b3c9066275c
awhb <awhangbin@gmail.com>
2025-11-06 20:10:13 +0800

    chore: update deprecated zod call

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -22,7 +22,7 @@ const formSchema = z
     username: z
       .string()
       .min(2, { message: "Username must be at least 2 characters." }),
-    email: z.string().email({ message: "Please enter a valid email." }),
+    email: z.email({ message: "Please enter a valid email." }),
     password: z
       .string()
       .min(8, { message: "Password must be at least 8 characters." }) 
