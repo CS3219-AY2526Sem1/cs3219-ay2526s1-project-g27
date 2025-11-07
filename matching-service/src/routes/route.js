21f2c098533c392196714bb0f0fa81dc286e523b
sk2001git <e0958427@u.nus.edu>
2025-11-07 21:22:06 +0800

    Try resolve merge conflict

--- matching-service/src/routes/route.js
+++ matching-service/src/routes/route.js
@@ -15,7 +15,7 @@ matchingRouter.post("/queue", async(req, res) => {
         if (!SSEClientConnection) {
             throw new Error();
         }
-        const question = await axios.get("http://question-service:3013/question/random", { categories: [userData.topic], difficulty: userData.topic});
+        const question = await axios.post("http://question-service:3013/question/random", { categories: [userData.topic], difficulty: userData.topic});
         if (!question) {
             SSEClientConnection.send("noQuestion", { message: "No question available for selected category and difficulty. Please make another selection." });
             await handleDisconnect(userData.userId, matchingQueue);
